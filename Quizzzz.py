8efbed0933831821ac6c94612c7ba7bfa807137a
Tobias Graml <tobias.graml@ben-energy.com>
2015-07-26 22:24:57 +0200

    Refactored by grouping some code in methods and introducing logging statements

--- Quizzzz.py
+++ Quizzzz.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 __author__ = 'franzgraml'
 
 import nltk, re, pprint
@@ -5,6 +6,7 @@ import nltk.data
 import urllib.request
 import pickle
 import random
+import logging
 from bs4 import BeautifulSoup
 
 
@@ -13,134 +15,149 @@ class Frageundantwort(object):
     def __init__(self, textsource, begriff):
         self.textsource = textsource
         self.begriff = begriff
-        ##print('textsource = ',textsource)
-        ##print('begriff = ',begriff)
+        
+        logging.debug('textsource = ',textsource)
+        logging.debug('begriff = ',begriff)
 
         # Speichere HTML-Dokument vom Internet auf lokale Binärdatei 
         # bei Tests nur 1 mal durchführen,
         # dann kann mit lokaler Datei weiterexperimentiert werden
 
-        # Lesen aus Textquelle
-        url = textsource+begriff
-        ##html = urllib.request.urlopen(url).read() # Alles einlesen
-        html = urllib.request.urlopen(url).read(20000) # Nur einen Teil einlesen
-        f = open('daten.dmp','wb')
-        pickle.dump(html,f)
-        f.close()
+        decoded = self.read_from_web(begriff, textsource)
 
-        # Einlesen der Binärdatei
-        f = open('daten.dmp','rb')
-        htmlsaved = pickle.load(f)
-        f.close()
+        absatzlist = self.extractParagraphs(decoded)
 
-        # Umwandlung in 'class str'
-        decoded = htmlsaved.decode("utf-8")
-        ##decoded = """<p>Satz 1</p><p>Satz 2</p><p>Satz 3</p>""" # Nur zum Test
-
-        ##print(decoded[:500]) # zum Test
-        ##print(decoded) # zum Test
-        ##print('============')
-
-        l = len(decoded)
-        ##print('============')
-        ##print('len(decoded) = ',l) # nur zum Test
-        ##print('============')
-
-
-        # Absatzanfänge suchen
-        ##print('decoded = ',decoded)
-        texta = decoded
-        ##print('texta initial = ',texta)
-        mla = [] # ml = Matchliste Satzanfang
-        gefa = 0 # Initialisieren Hilfszähler
-        x = '<p>' # Gesuchter Ausdruck
-        lx = len(x)
-        ##print('len(x) = ',len(x))
-        for i in range(l): # Alle Positionen von x in eine Liste schreiben
-            gef = texta.find(x)
-            gefn = gefa+gef
-            if gef != -1:
-                mla.append(gefn)
-                texta = texta[gef+lx:]
-                ##print('texta ',i,':',texta)
-                gefa = gefn
-        ##print('Absatzanfangsliste = ',mla) # nur zum Test
-
-        # Absatzenden suchen
-        textb = decoded
-        ##print('textb initial = ',textb)
-        mle = [] # ml = Matchliste Satzende
-        gefa = 0 # Initialisieren Hilfszähler
-        y = '</p>' # Gesuchter Ausdruck
-        ly = len(y)
-        for j in range(l): # Alle Positionen von y in eine Liste schreiben
-            gef = textb.find(y)
-            gefn = gefa+gef
-            if gef != -1:
-                mle.append(gefn)
-                textb = textb[gef+ly:]
-                ##print('textb ',j,':',textb)
-                gefa = gefn
-        ##print('Absatzendeliste = ',mle) # nur zum Test
-
-        # Absätze ohne Markups in Liste schreiben
-        absatzlist = []
-        ##for i in range(len(mla)): # Achtung: falls Absatz abgeschnitten wurde: indexüberlauf!!!
-        for i in range(len(mle)): # mle kann kürzer sein als mla, wenn Absatz abgeschnitten wurde
-            absatzanfang = mla[i]
-            absatzende = mle[i] 
-            absatz = decoded[absatzanfang+lx:absatzende]
-            ##print('============')
-            ##print('Absatz',i,':', absatz)
-            ##print('============')
-            # Absätze von Markups reinigen
-            cabsatz = BeautifulSoup(absatz, "html.parser").get_text() # Entfernen der Markups
-            ##print('============')
-            ##print('cabsatz = ',cabsatz)
-            ##print('============')
-            absatzlist.append(cabsatz)
-        ##print('============')
-        ##print(absatzlist)
-
-
-        # Sätze aus den einzelnen Absätzen bilden
-        sentencelist = []
-        sent_detector = nltk.data.load('tokenizers/punkt/english.pickle') # Sentencer laden
-
-        for k in range(len(absatzlist)):
-            satzlist = sent_detector.tokenize(absatzlist[k].strip())
-            #Achtung: satz ergibt eine Liste von Sätzen!!!
-            ##print('satzlist',k,':',satzlist)
-            sentencelist.append(satzlist)
-        ##print('sentencelist = ',sentencelist) # Achtung: sentencelist ist eine liste von Listen von Sätzen!!!
+        sentencelist = self.extractSentences(absatzlist)
 
         # Bestimmten Ausdruck im Satz suchen
         t = ' is a '
 
+        self.generateQuestionAndAnswers(sentencelist, t)
+
+    def generateQuestionAndAnswers(self, sentencelist, t):
         qcounter = 0
-        index = -1 # Index zurücksetzen
+        index = -1  # Index zurücksetzen
         # Einzelne Sätze auslesen
         for i in range(len(sentencelist)):
             if qcounter > 0: break
             for j in range(len(sentencelist[i])):
                 satz = sentencelist[i][j]
-                ##print('============')
-                ##print('sentencelist[',i,',[',j,'] = ', satz)
-                index = satz.find(t) # Ausdruck t im Satz finden
-                ##print('index =',index)
-                if index == -1: break   # Wenn Ausdruck nicht enthalten, Abbruch
-                                        # und neue Internetseite laden
-                    
-                if index != -1: # Nur wenn wirklich der Ausdruck im Satz enthalten ist
-                    question = satz[:index+len(t)-1] # Frage generieren und ausgeben
+                logging.debug('============')
+                logging.debug('sentencelist[', i, ',[', j, '] = ', satz)
+                index = satz.find(t)  # Ausdruck t im Satz finden
+                logging.debug('index =', index)
+                if index == -1: break  # Wenn Ausdruck nicht enthalten, Abbruch
+                # und neue Internetseite laden
+
+                if index != -1:  # Nur wenn wirklich der Ausdruck im Satz enthalten ist
+                    question = satz[:index + len(t) - 1]  # Frage generieren und ausgeben
                     questionlist.append(question)
-                    ##print(question + ':')
-                    answer = satz[index+len(t):] # richtige Antwort generieren und ausgeben
+                    logging.debug(question + ':')
+                    answer = satz[index + len(t):]  # richtige Antwort generieren und ausgeben
                     answer = answer.rstrip('.')
                     answerlist.append(answer)
-                    qcounter = qcounter+1 # Nur 1 Frage generieren
+                    qcounter = qcounter + 1  # Nur 1 Frage generieren
                     if qcounter > 0: break
-      
+
+    def extractSentences(self, absatzlist):
+        # Sätze aus den einzelnen Absätzen bilden
+        sentencelist = []
+        sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')  # Sentencer laden
+        for k in range(len(absatzlist)):
+            satzlist = sent_detector.tokenize(absatzlist[k].strip())
+            # Achtung: satz ergibt eine Liste von Sätzen!!!
+            logging.debug('satzlist',k,':',satzlist)
+            sentencelist.append(satzlist)
+        logging.debug('sentencelist = ',sentencelist) # Achtung: sentencelist ist eine liste von Listen von Sätzen!!!
+        return sentencelist
+
+    def extractParagraphs(self, decoded):
+        l = len(decoded)
+        logging.debug('============')
+        logging.debug('len(decoded) = ', l)  # nur zum Test
+        logging.debug('============')
+
+        # Absatzanfänge suchen
+        texta = decoded
+        logging.debug('texta initial = ',texta)
+        mla = []  # ml = Matchliste Satzanfang
+        gefa = 0  # Initialisieren Hilfszähler
+        x = '<p>'  # Gesuchter Ausdruck
+        lx = len(x)
+        logging.debug('len(x) = ',len(x))
+
+        # Alle Positionen von x in eine Liste schreiben
+        for i in range(l):
+            gef = texta.find(x)
+            gefn = gefa + gef
+            if gef != -1:
+                mla.append(gefn)
+                texta = texta[gef + lx:]
+                logging.debug('texta ',i,':',texta)
+                gefa = gefn
+        logging.debug('Absatzanfangsliste = ',mla) # nur zum Test
+
+        # Absatzenden suchen
+        textb = decoded
+        logging.debug('textb initial = ',textb)
+        mle = []  # ml = Matchliste Satzende
+        gefa = 0  # Initialisieren Hilfszähler
+        y = '</p>'  # Gesuchter Ausdruck
+        ly = len(y)
+        for j in range(l):  # Alle Positionen von y in eine Liste schreiben
+            gef = textb.find(y)
+            gefn = gefa + gef
+            if gef != -1:
+                mle.append(gefn)
+                textb = textb[gef + ly:]
+                logging.debug('textb ',j,':',textb)
+                gefa = gefn
+        logging.debug('Absatzendeliste = ',mle) # nur zum Test
+
+        # Absätze ohne Markups in Liste schreiben
+        absatzlist = []
+        ##for i in range(len(mla)): # Achtung: falls Absatz abgeschnitten wurde: indexüberlauf!!!
+        for i in range(len(mle)):  # mle kann kürzer sein als mla, wenn Absatz abgeschnitten wurde
+            absatzanfang = mla[i]
+            absatzende = mle[i]
+            absatz = decoded[absatzanfang + lx:absatzende]
+            logging.debug('============')
+            logging.debug('Absatz',i,':', absatz)
+            logging.debug('============')
+            # Absätze von Markups reinigen
+            cabsatz = BeautifulSoup(absatz, "html.parser").get_text()  # Entfernen der Markups
+            logging.debug('============')
+            logging.debug('cabsatz = ',cabsatz)
+            logging.debug('============')
+            absatzlist.append(cabsatz)
+
+        logging.debug('============')
+        logging.debug(absatzlist)
+        return absatzlist
+
+    def read_from_web(self, begriff, textsource):
+        # Lesen aus Textquelle
+        url = textsource + begriff
+        ##html = urllib.request.urlopen(url).read() # Alles einlesen
+        html = urllib.request.urlopen(url).read(20000)  # Nur einen Teil einlesen
+        f = open('daten.dmp', 'wb')
+        pickle.dump(html, f)
+        f.close()
+        # Einlesen der Binärdatei
+        f = open('daten.dmp', 'rb')
+        htmlsaved = pickle.load(f)
+        f.close()
+        # Umwandlung in 'class str'
+        decoded = htmlsaved.decode("utf-8")
+        ##decoded = """<p>Satz 1</p><p>Satz 2</p><p>Satz 3</p>""" # Nur zum Test
+        
+        logging.debug('decoded = ',decoded)
+        logging.debug(decoded[:500]) # zum Test
+        logging.debug('============')
+        
+        return decoded
+
+
 # Programmablaufteil
 questionlist = []
 answerlist = []
@@ -176,12 +193,12 @@ print(questionlist[0] + ':')
 
 # Merken, welche Antwort die richtige ist
 korranswer = answerlist[0]
-##print('korranswer = ',korranswer)
+logging.debug('korranswer = ',korranswer)
 
 ### Antworten ohne Würfeln ausgeben (zum Test); die erste ist immer die richtige
-##print ('a)', answerlist[0])
-##print ('b)', answerlist[1])
-##print ('c)', answerlist[2])
+logging.debug ('a)', answerlist[0])
+logging.debug ('b)', answerlist[1])
+logging.debug ('c)', answerlist[2])
 
 # Den Antworten true-false-Attribute hinzufügen
 tflist = [] #true false Hilfs-Liste
@@ -190,17 +207,17 @@ f = 'false'
 tflist.append([answerlist[0],t])
 tflist.append([answerlist[1],f])
 tflist.append([answerlist[2],f]) 
-##print(tflist)
+logging.debug(tflist)
 
 # Antworten durcheinanderwürfeln und ausgeben
 s = [0,1,2]
 random.shuffle(s) # << shuffle before print or assignment. funktioniert das Würfeln wirklich???
-##print(s)
+logging.debug(s)
 
 ### Antworten mit true/false-Marker ausgeben, zum Test
-##print ('a)', tflist[s[0]]) 
-##print ('b)', tflist[s[1]])
-##print ('c)', tflist[s[2]])
+logging.debug ('a)', tflist[s[0]]) 
+logging.debug ('b)', tflist[s[1]])
+logging.debug ('c)', tflist[s[2]])
 
 print ('a)', tflist[s[0]][0])
 print ('b)', tflist[s[1]][0])
@@ -212,7 +229,7 @@ print('==================')
 x = input('Welche Antwort ist richtig? Bitte geben Sie a, b oder c ein.   ')
 print('==================')
 print('Ihr Tipp ist: ',x)
-##print('type(x) = ',type(x))
+logging.debug('type(x) = ',type(x))
 
 
 # Buchstaben zu Index machen. 
@@ -221,7 +238,7 @@ elif x == 'b': ndex=1
 elif x == 'c': ndex=2
 else:
     ndex = -1 
-##print('ndex = ',ndex)
+logging.debug('ndex = ',ndex)
 
 # Richtige Antwort gewählt?
 if ndex == -1:
